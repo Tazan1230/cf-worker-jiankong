f8e5851f117d2d8c6ef0a4bcebd6097f3f6eb353
Veryha <52478719+Tazan1230@users.noreply.github.com>
2025-09-17 09:41:25 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -2,12 +2,12 @@ import { MaintenanceConfig, PageConfig, WorkerConfig } from './types/config'
 
 const pageConfig: PageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "cf-jiankong-page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://github.com/Tazan1230', label: 'GitHub' },
+    { link: 'https://pigpiggo.pp.ua/', label: 'TV' },
+    { link: 'ukxd001@gmail.com', label: 'Email Me', highlight: true },
   ],
   // [OPTIONAL] Group your monitors
   // If not specified, all monitors will be shown in a single list
